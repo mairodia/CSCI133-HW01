7f99a5c5fc894a94543a04c872ec7229976f20dc
mairodia <32186812+mairodia@users.noreply.github.com>
2018-02-08 19:53:36 -0800

    Create cname.cpp

---
+++ cname.cpp
@@ -0,0 +1,57 @@
+// ===========================================================================================
+// File: cname.cpp (Spring 2018)
+// ===========================================================================================
+// Programmer: Jennifer King
+// Professor: Scott Edwards
+// Date: 2/5/2018
+// Class: CSCI 133F | T/R 9:30AM
+// Description:
+// 		This program displays nameOne, nameTwo, and nameThree. It then recieves a name as
+// 		input and displays that name. nameThree is assigned a value, and the contents of the
+// 		name.dat file are displayed.
+// ===========================================================================================
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <csting>
+#include "cname.h"
+using namespace std;
+
+CName::CName() //constructor
+{
+	for(int i=0; i < NAMELEN; i++)
+	{
+		m_first[i] = '\0'
+		m_last[i] = '\0'
+	}
+}
+
+CName::CName(const char* first, const char* last) //constructor
+{
+	strcpy(m_first, first);
+	strcpy(m_last, last);
+}
+
+// == WriteFullName =========================================================================
+// This function outputs the first and last names.
+//
+// Input:
+// 		N/A
+//
+// Output:
+// 		void
+// ==========================================================================================
+void CName::WriteFullName() const
+{
+	for(int i=0; i < NAMELEN; i++)
+	{
+		cout << m_first[i];
+	}
+	cout << " ";
+	for(int x=0; x < NAMELEN; x++)
+	{
+		cout << m_last[x];
+	}
+	cout << endl;
+}
